e607b76d69887ec78a576ffc80abe29d347c05e9
Louis <light.louis@hotmail.fr>
2024-04-06 18:27:21 +0200

    Ajout varibales globales

--- loup-garou-app/tailwind.config.js
+++ loup-garou-app/tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      'dark-bg': '#011C27',
+      'dark-1': '#0E223A',
+      'primary': '#D90429',
+      'white': '#F5F0F6',
+      'black': '#000000',
+      'dark-2': '#03254E',
+    },
   },
   plugins: [],
 }
