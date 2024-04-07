1eea0b3f60055e1ebc4dbc8a582edb1450e05006
julien ratouit <77002747+julien-ratouit@users.noreply.github.com>
2024-04-07 05:10:19 +0200

    a l'aide

--- loup-garou-app/src/app/components/shared/equilibrage/equilibrage.component.ts
+++ loup-garou-app/src/app/components/shared/equilibrage/equilibrage.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ROLES, ROLES_LIST } from 'src/app/constants/roles.constants';
+import { Role } from 'src/app/models/role.model';
+import { RolesService } from 'src/app/services/roles.service';
 
 @Component({
   selector: 'app-equilibrage',
@@ -6,8 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./equilibrage.component.scss']
 })
 export class EquilibrageComponent {
+  roles!: Role[];
+  loups: number = 5;
+  villageois: number = 8;
 
-  constructor(){}
-
-  
+  constructor(private service: RolesService){
+    let element = document.getElementById("bar")!;
+    if (element) {
+      // Définissez la largeur en utilisant la propriété style.width
+      element.style.width = ((this.loups/this.villageois)*100).toString();
+    }
+    this.service.selectedRolesArray$.subscribe(value => {
+      this.roles = value;
+      this.loups = this.roles.at(this.roles.findIndex(role => role.name == ROLES_LIST.LOUP_GAROU.name))?.quantity!;
+      this.villageois = this.roles.at(this.roles.findIndex(role => role.name == ROLES_LIST.VILLAGEOIS.name))?.quantity!;
+      let element = document.getElementById("bar")!;
+      if (element) {
+        // Définissez la largeur en utilisant la propriété style.width
+        element.style.width = ((this.loups/this.villageois)*0).toString();
+      }
+    });
+  }
 }
