04b61a6f0e457a62a75fbec05aff3fc068ff2925
CharlesRenoit <renoit-charles@outlook.fr>
2024-04-07 05:13:13 +0200

    Feu de camp - component

--- loup-garou-app/src/app/components/shared/feu-de-camp/feu-de-camp.component.ts
+++ loup-garou-app/src/app/components/shared/feu-de-camp/feu-de-camp.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./feu-de-camp.component.scss']
 })
 export class FeuDeCampComponent {
+  players = ['Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5', 'Player 5', 'Player 5', 'Player 5']; // Vous pouvez remplacer cela par votre propre liste de joueurs
 
+  calculateRotation(index: number): number {
+    const totalPlayers = this.players.length;
+    const degreesPerPlayer = 360 / totalPlayers;
+    return index * degreesPerPlayer;
+  }
+
+  getInfosUser(player:)
 }
