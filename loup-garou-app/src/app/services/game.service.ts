53c0755fc8ccc7e51011619146fcd1389f6b4fe2
Louis <light.louis@hotmail.fr>
2024-04-07 06:24:04 +0200

    ajout service game

---
+++ loup-garou-app/src/app/services/game.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Joueur } from '../models/joueur.model';
+import { HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+
+  apiUrl = ``;
+
+  constructor(private http: HttpClient) { }
+
+  getStart(): Observable<any> {
+    const pseudos = [
+      'Ronteah', 'Stun', 'Hugod', 'Chrakos', 'Retweet', 'Clement C', 'Geekswy'
+    ];
+    const params = new HttpParams().set('players', JSON.stringify(pseudos)).set('roles', JSON.stringify({"loup": 2, "villageois": 5}));
+    return this.http.get<any>(this.apiUrl, { params });
+  }
+
+}
